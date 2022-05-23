4b5fd4861186506d1b9385317fe92a8b6350ffa9
Adarsh <adarsh@healthcubed.com>
2022-05-23 15:53:14 +0530

    changes made

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
 #include stdio.h //adding header file by juan
+
+/*Added by Adarsh*/
+void main()
+{
+	LED=~LED;
+
+}
--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -75,7 +75,7 @@ void app_main(void)
 
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
+        blink_led(); //Blink LED
         /* Toggle the LED state */
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
